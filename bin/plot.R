a5b39fd502674e4a51b36dcf2403673b5ea4c4eb
Andrey Hoglund <andrey.hoglund@protonmail.com>
2024-11-26 11:26:56 +0100

    bin/plot.R added, produce wordcloud figure as pdf, png

---
+++ bin/plot.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+
+#library(RColorBrewer)
+library(webshot)
+#webshot::install_phantomjs()
+library(htmlwidgets)
+library(wordcloud2)
+
+words <- read.csv("data/wordcloud.csv")
+
+colnames(words) <- c("freq", "word")
+words <- words[, c("word", "freq")]
+
+# remove mixture & cells
+rows <- words$word %in% c("mixture", "cells")
+words <- words[!rows, ]
+
+set.seed(13337)
+
+fig <- wordcloud2(
+	data = words,
+	size = 1.4,
+	#shape = 'diamond', # square
+	backgroundColor = "transparent",
+	#fontFamily = 
+	color = "random-dark"
+)
+
+# export
+saveWidget(fig, "fig.html", selfcontained = TRUE)
+# and in png or pdf
+webshot("fig.html", "fig.pdf", delay = 5, vwidth = 2000, vheight = 700)
+webshot("fig.html", "fig.png", delay = 5, vwidth = 2000, vheight = 700)
+
+cat(paste0(
+  "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n",
+  "./bin/plot.R complete\n",
+  "./fig.pdf & ./fig.png generated",
+  "\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"
+))
